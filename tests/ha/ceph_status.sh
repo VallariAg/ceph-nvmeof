6518aa2cd21ee52195a5e90eed5ae7573be85cef
Vallari Agrawal <val.agl002@gmail.com>
2024-09-05 12:54:34 +0530

    ceph status fix
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- tests/ha/ceph_status.sh
+++ tests/ha/ceph_status.sh
@@ -4,9 +4,16 @@ POOL="${RBD_POOL:-rbd}"
 CEPH_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | grep -v nvme | grep ceph | awk '{print $1}')
 GW1_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /1/ {print $1}')
 
+docker compose exec -T ceph ceph status
+# docker exec $CEPH_NAME ceph status
+
+# echo "ℹ️  Step 1: verify nvmeof deployed"
+
+# docker compose exec -T ceph ceph status | grep "nvmeof"
+
 echo "ℹ️  Step 1: verify 2 gateways"
 
-docker exec $CEPH_NAME ceph status | grep "nvmeof" | grep "2 gateway active"
+docker compose exec -T ceph ceph status | grep "nvmeof" | grep "2 gateway active"
 
 echo "ℹ️  Step 2: stop a gateway"
 
@@ -16,4 +23,4 @@ sleep 5
 
 echo "ℹ️  Step 3: verify 1 gateway"
 
-docker exec $CEPH_NAME ceph status | grep "nvmeof" | grep "1 gateway active"
+docker compose exec -T ceph ceph status | grep "nvmeof" | grep "1 gateway active"
