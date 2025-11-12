335debe66ce8b897dbe0750016336c9aae724711
Vallari Agrawal <val.agl002@gmail.com>
2025-11-12 11:14:55 +0530

    GA: make baisic test work
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- tests/ha/auto_listeners.sh
+++ tests/ha/auto_listeners.sh
@@ -4,177 +4,135 @@ set -xe
 
 GW1_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /1/ {print $1}')
 GW2_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /2/ {print $1}')
-# GW3_NAME=$(docker ps --format '{{.ID}}\t{{.Names}}' | awk '$2 ~ /nvmeof/ && $2 ~ /3/ {print $1}')
 
 ip1="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$GW1_NAME")"
 ip2="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$GW2_NAME")"
-#ip3="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$GW3_NAME")"
 
-NUM_SUBSYSTEMS=2
-NQN1="nqn.2016-06.io.spdk:cnode01"
-NQN2="nqn.2016-06.io.spdk:cnode02"
-# NQN3="nqn.2016-06.io.spdk:cnode3"
+NUM_SUBSYSTEMS=3
+NQN1="nqn.2016-06.io.spdk:cnode01" # auto-listeners 
+NQN2="nqn.2016-06.io.spdk:cnode02" # auto-listeners with secure listeners 
+NQN3="nqn.2016-06.io.spdk:cnode03" # normal listeners
 
 SUBNET=$(echo $ip1 | grep -oE "^([0-9]{1,3}\.){3}")
 SUBNET="${SUBNET}0/24"
 
-echo "Subnet $SUBNET would be used to create 2 subsystems $NQN1 and $NQN2"
-
-# HOSTNQN1="${NQN1}:host"
-# HOSTNQN2="${NQN2}:host"
+echo "Subnet $SUBNET would be used to create 2 subsystems with auto-listeners $NQN1 (non-secure listeners) and $NQN2 (secure listeners)"
+echo "And create $NQN3 with normal listeners"
 
 
-
-test_auto_listeners()
- {  
-   # Parameters: secure (boolean)
-
-   # CHECK 1: list listeners
-    for i in $(seq $NUM_SUBSYSTEMS); do
+test_listeners()
+ {
+   ip_1=$1
+   ip_2=$2 # optional
+   for i in $(seq $NUM_SUBSYSTEMS); do
       NQN="nqn.2016-06.io.spdk:cnode0$i"
-      docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n $NQN > /tmp/listeners.txt 
-      # listener_list=$(docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n $NQN)
-      # echo "list listeners: "  $listener_list
+      is_secure=false
+      if [ "$NQN" -eq "$NQN2" ]; then
+         is_secure=true 
+      fi
+      is_manual=false
+      if [ "$NQN" -eq "$NQN3" ]; then
+         is_manual=true
+      fi 
 
+      # CHECK 1: list listeners
+      docker compose run -T --rm nvmeof-cli --server-address $ip_1 --server-port 5500 --output stdio --format json listener list -n $NQN > /tmp/listeners.txt 
       cat /tmp/listeners.txt
       [[ `cat /tmp/listeners.txt | jq -r '.status'` == "0" ]]
       [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].trtype'` == "TCP" ]]
       [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].adrfam'` == "ipv4" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].traddr'` == "$ip1" ]]
+      [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].traddr'` == "$ip_1" ]]
       [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].trsvcid'` == "4420" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].secure'` == "false" ]]
+      [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].secure'` == "$is_secure" ]]
       [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].active'` == "true" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].manual'` == "false" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].trtype'` == "TCP" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].adrfam'` == "ipv4" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].traddr'` == "$ip2" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].trsvcid'` == "4420" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].secure'` == "false" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].active'` == "true" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].manual'` == "false" ]]
-      [[ `cat /tmp/listeners.txt | jq -r '.listeners[2]'` == "null" ]]
-      hostname20=`cat /tmp/listeners.txt | jq -r '.listeners[0].host_name'`
-      hostname21=`cat /tmp/listeners.txt | jq -r '.listeners[1].host_name'`
-      hostname22=`cat /tmp/listeners.txt | jq -r '.listeners[2].host_name'`
+      [[ `cat /tmp/listeners.txt | jq -r '.listeners[0].manual'` == "$is_manual" ]]
+      if [ -n "$ip_2" ]; then
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].trtype'` == "TCP" ]]
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].adrfam'` == "ipv4" ]]
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].traddr'` == "$ip_2" ]]
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].trsvcid'` == "4420" ]]
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].secure'` == "$is_secure" ]]
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].active'` == "false" ]]
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1].manual'` == "$is_manual" ]]
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[2]'` == "null" ]]
+      else
+         [[ `cat /tmp/listeners.txt | jq -r '.listeners[1]'` == "null" ]]
+      fi
 
       # CHECK 2: gw listener_info
-      docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json gw listener_info -n $NQN > /tmp/listeners.txt 
+      docker compose run -T --rm nvmeof-cli --server-address $ip_1 --server-port 5500 --output stdio --format json gw listener_info -n $NQN > /tmp/gw_listeners.txt
       cat /tmp/gw_listeners.txt
       [[ `cat /tmp/gw_listeners.txt | jq -r '.status'` == "0" ]]
       [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.trtype'` == "TCP" ]]
       [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.adrfam'` == "ipv4" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.traddr'` == "2001:db8::3" ]]
+      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.traddr'` == "$ip_1" ]]
       [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.trsvcid'` == "4420" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.secure'` == "false" ]]
+      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.secure'` == "$is_secure" ]]
       [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.active'` == "true" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1].listener.trtype'` == "TCP" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1].listener.adrfam'` == "ipv4" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1].listener.traddr'` == "0.0.0.0" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1].listener.trsvcid'` == "4430" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1].listener.secure'` == "false" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1].listener.active'` == "true" ]]
-      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[2]'` == "null" ]]
+      [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1]'` == "null" ]]
 
+      if [ -n "$ip_2" ]; then
+         docker compose run -T --rm nvmeof-cli --server-address $ip_2 --server-port 5500 --output stdio --format json gw listener_info -n $NQN > /tmp/gw_listeners.txt
+         cat /tmp/gw_listeners.txt
+         [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.trtype'` == "TCP" ]]
+         [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.adrfam'` == "ipv4" ]]
+         [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.traddr'` == "$ip_2" ]]
+         [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.trsvcid'` == "4420" ]]
+         [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.secure'` == "$is_secure" ]]
+         [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[0].listener.active'` == "true" ]]
+         [[ `cat /tmp/gw_listeners.txt | jq -r '.gw_listeners[1]'` == "null" ]]
+      fi
+      # CHECK 3: nvme discover check
+      # CHECK 4: nvme connect+list check
 
-    done
-    min_val=10000
-    max_val=0
-    for group in "${!ana_load[@]}"; do
-       val=${ana_load[$group]}
-       if (( val < min_val )); then
-          min_val=$val
-       fi
-       if (( val > max_val )); then
-          max_val=$val
-       fi
-    done
-    if (( max_val - min_val > 2 )); then
-       echo "ℹ️ ℹ️ Namespace ANA group Distribution issue"
-       exit 1
-    else
-       echo "ℹ️ ℹ️ ANA Compared OK!"
-    fi
+   done
 }
 
+# TEST 1: create auto-listeners and verify
+echo "ℹ️ ℹ️ Start test (setup step):  create 2 subsystems with auto listeners and 1 normal subsystem with manual listeners:"
 
-echo "ℹ️ ℹ️ Start test:  create additional 2 subsystems with auto listeners:"
+docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 subsystem add -n $NQN1 --no-group-append --network-mask $SUBNET
+docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 subsystem add -n $NQN2 --no-group-append --network-mask $SUBNET --secure-listeners
 
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 subsystem add -n $NQN1 --no-group-append --network-mask $SUBNET --no-group-append
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 subsystem add -n $NQN2 --no-group-append --network-mask $SUBNET --secure-listeners --no-group-append
+docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 subsystem add -n $NQN3 --no-group-append
+docker compose run --rm nvmeof-cli --server-address $ip2  --server-port 5500 listener add  --subsystem $NQN3 --host-name $GW1_NAME --traddr $ip1 --trsvcid 4420
+docker compose run --rm nvmeof-cli --server-address $ip2  --server-port 5500 listener add  --subsystem $NQN3 --host-name $GW2_NAME --traddr $ip2 --trsvcid 4420
 
-#docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 subsystem add -n $NQN3 --no-group-append
-sleep 2
-# docker compose run --rm nvmeof-cli --server-address $ip1 --server-port 5500 host add --subsystem $NQN1 --host-nqn "*"
-# docker compose run --rm nvmeof-cli --server-address $ip1 --server-port 5500 host add --subsystem $NQN2 --host-nqn "*"
+docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json subsystem list
 
-# docker compose  run --rm nvmeof-cli --server-address $ip1  --server-port 5500 listener add  --subsystem $NQN2 --host-name $GW1_NAME --traddr $ip1 --trsvcid 4420
-# docker compose  run --rm nvmeof-cli --server-address $ip1  --server-port 5500 listener add  --subsystem $NQN3 --host-name $GW1_NAME --traddr $ip1 --trsvcid 4420
-# docker compose  run --rm nvmeof-cli --server-address $ip2  --server-port 5500 listener add  --subsystem $NQN2 --host-name $GW2_NAME --traddr $ip2 --trsvcid 4420
-# docker compose  run --rm nvmeof-cli --server-address $ip2  --server-port 5500 listener add  --subsystem $NQN3 --host-name $GW2_NAME --traddr $ip2 --trsvcid 4420
-
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json subsystem list
-
+echo "ℹ️ ℹ️  Create hosts"
 for i in $(seq $NUM_SUBSYSTEMS); do
    NQN="nqn.2016-06.io.spdk:cnode0$i"
-   docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json listener list -n $NQN
-   docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n $NQN
-   docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json gw listener_info -n $NQN
+   docker compose run --rm nvmeof-cli --server-address $ip2 --server-port 5500 host add --subsystem $NQN --host-nqn ${NQN}host
 done
 
+echo "ℹ️ ℹ️  Create namespaces"
+for i in $(seq $NUM_SUBSYSTEMS); do
+   NQN="nqn.2016-06.io.spdk:cnode0$i"
+   for num in $(seq 3); do
+      image_name="demo_image$(expr \( $num + 5 \) \* $i)"
+      echo $image_name
+      docker compose  run --rm nvmeof-cli --server-address $ip2 --server-port 5500 namespace add --subsystem $NQN --rbd-pool rbd --rbd-image $image_name --size 10M --rbd-create-image --force
+   done
+done
 
-docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n "nqn.2016-06.io.spdk:cnode1"
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json gw listener_info -n "nqn.2016-06.io.spdk:cnode1"
-docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json gw listener_info -n "nqn.2016-06.io.spdk:cnode1"
-# echo "ℹ️ ℹ️  Create namespaces with explicit LB = 1"
+test_listeners $ip2 $ip1
 
-# for i in $(seq $NUM_SUBSYSTEMS); do
-#    NQN="nqn.2016-06.io.spdk:cnode$i"
-#    for num in $(seq $MAX_NAMESPACE);
-#    do
-#    image_name="demo_image$(expr \( $num + 5 \) \* $i)"
-#    echo $image_name
-#    docker compose  run --rm nvmeof-cli --server-address $ip2 --server-port 5500 namespace add --subsystem $NQN --rbd-pool rbd --rbd-image $image_name  --size 10M --rbd-create-image -l 1 --force
-#    done
-# done
 
-# echo "ℹ️ ℹ️  Wait for rebalance "
-# sleep 250
-
-# test_auto_listeners 2
+# TEST 2: scale-up / scale-down and verify
+echo "ℹ️ ℹ️ Test auto-listeners for scale-up and scale-down"
 
 docker compose exec -T ceph ceph nvme-gw delete $GW1_NAME rbd ''
-echo "ℹ️ ℹ️  Wait for scale-down rebalance "
+echo "ℹ️  Wait for scale-down"
 sleep 110
-# test_auto_listeners 1
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json subsystem list
-for i in $(seq $NUM_SUBSYSTEMS); do
-   NQN="nqn.2016-06.io.spdk:cnode0$i"
-   docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json listener list -n $NQN
-   docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n $NQN
-   docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json gw listener_info -n $NQN
-done
-docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n "nqn.2016-06.io.spdk:cnode1"
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json gw listener_info -n "nqn.2016-06.io.spdk:cnode1"
-docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json gw listener_info -n "nqn.2016-06.io.spdk:cnode1"
 
-
-
+test_listeners $ip2
 
 docker compose exec -T ceph ceph nvme-gw create $GW1_NAME rbd ''
-echo "ℹ️ ℹ️  Wait for rebalance after create GW"
+echo "ℹ️ Wait for scale up"
 sleep 200
-# test_auto_listeners 2
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json subsystem list
-for i in $(seq $NUM_SUBSYSTEMS); do
-   NQN="nqn.2016-06.io.spdk:cnode0$i"
-   docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json listener list -n $NQN
-   docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n $NQN
-   docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json gw listener_info -n $NQN
-done
-docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json listener list -n "nqn.2016-06.io.spdk:cnode1"
-docker compose run -T --rm nvmeof-cli --server-address $ip1 --server-port 5500 --output stdio --format json gw listener_info -n "nqn.2016-06.io.spdk:cnode1"
-docker compose run -T --rm nvmeof-cli --server-address $ip2 --server-port 5500 --output stdio --format json gw listener_info -n "nqn.2016-06.io.spdk:cnode1"
-#
+test_listeners $ip2 $ip1
+
 
 ############################################################################################
 
