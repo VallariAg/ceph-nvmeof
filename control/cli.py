38daeae00a151aa66e06c4c5c1eaa45f84a93baf
Vallari Agrawal <val.agl002@gmail.com>
2025-12-05 16:32:48 +0530

    test
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- control/cli.py
+++ control/cli.py
@@ -1153,7 +1153,7 @@ class GatewayClient:
                  help="For this subnet, automatically create listeners for this subsystem",
                  required=False),
         argument("--secure-listeners",
-                 help="Make all the auto-listeners for this subsystem secures",
+                 help="Make all the auto-listeners for this subsystem secure",
                  action='store_true',
                  required=False),
     ]
