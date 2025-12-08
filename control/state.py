af031765c0db889eb1fcb95507b843f88dda4c3b
Vallari Agrawal <val.agl002@gmail.com>
2025-12-08 09:16:44 +0530

    Minor fixes on auto-listeners
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- control/state.py
+++ control/state.py
@@ -1505,6 +1505,7 @@ class GatewayStateHandler:
                 GatewayState.NAMESPACE_QOS_PREFIX,
                 GatewayState.NAMESPACE_HOST_PREFIX,
                 GatewayState.LISTENER_PREFIX,
+                GatewayState.SUBSYSTEM_NETWORK_MASK,
             ]
 
             if not self.omap.ioctx:
@@ -1792,8 +1793,6 @@ class GatewayStateHandler:
                    len(auto_listener_add) > 0:
                     grouped_changed = self._group_by_prefix(changed, prefix_list)
 
-                    if len(auto_listener_add) > 0:
-                        prefix_list += [GatewayState.SUBSYSTEM_NETWORK_MASK]
                     if len(only_subsystem_key_changed) > 0:
                         prefix_list += [GatewayState.SUBSYSTEM_KEY_PREFIX]
                     if len(ns_lb_group_changed) > 0:
